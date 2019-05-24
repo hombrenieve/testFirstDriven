7772b9039d6080502b8b125b10443a9591e6fda2
Pablo Rebaque <pablo.rebaque.diaz@gmail.com>
2019-05-24 19:33:23 +0200

    CardStack with composition in Waste

--- testFirstDriven/src/test/java/klondike/models/WasteTest.java
+++ testFirstDriven/src/test/java/klondike/models/WasteTest.java
@@ -1,10 +1,68 @@
 package klondike.models;
 
-public class WasteTest extends CardStackTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-	@Override
-	protected CardStack createCardStack() {
-		return new Waste();
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import klondike.models.builders.CardBuilder;
+
+public class WasteTest {
+	
+	protected List<Card> getCards(){
+		List<Card> cards = new ArrayList<Card>();
+		cards.add(new CardBuilder().build());
+		cards.add(new CardBuilder().build());
+		return cards;
+	}
+	
+	@Test
+	public void testEmptyWithEmpty() {
+		Waste waste = new Waste();
+		assertTrue(waste.empty());
+	}
+	
+	@Test
+	public void testEmptyWithNotEmpty() {
+		Waste waste = new Waste();
+		waste.push(this.getCards().get(0));
+		assertFalse(waste.empty());
+	}
+
+	@Test
+	public void testPushWithEmpty() {
+		Waste waste = new Waste();
+		waste.push(this.getCards().get(0));
+		assertEquals(this.getCards().get(0), waste.peek());
+	}
+	
+	@Test
+	public void testPushWithNotEmpty() {
+		Waste waste = new Waste();
+		waste.push(this.getCards().get(0));
+		waste.push(this.getCards().get(1));
+		assertEquals(this.getCards().get(1), waste.peek());
+	}
+
+	@Test
+	public void testPopEmpty() {
+		Waste waste = new Waste();
+		waste.push(this.getCards().get(0));
+		assertEquals(this.getCards().get(0), waste.pop());
+		assertTrue(waste.empty());
+	}
+	
+	@Test
+	public void testPopNotEmpty() {
+		Waste waste = new Waste();
+		waste.push(this.getCards().get(0));
+		waste.push(this.getCards().get(1));
+		assertEquals(this.getCards().get(1), waste.pop());
+		assertEquals(this.getCards().get(0), waste.peek());
 	}
 	
 }
